e9d3ae9709071caaaf141966f212dc062e299e6c
Mostafa El_sayed <mostafael_sayed@Mostafas-MacBook-Pro.local>
2017-06-06 19:55:18 +0530

    Initial Commit

---
+++ YallaFifa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YallaFifa
+//
+//  Created by Mostafa El_sayed on 6/6/17.
+//  Copyright © 2017 TheGang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
