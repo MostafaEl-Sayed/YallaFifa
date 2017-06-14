96ecf3f80c43054a1dd38c07e393352cb792d1da
Mostafa El_sayed <mostafael_sayed@mostafas-macbook-pro.local>
2017-06-14 17:42:01 +0530

    match req

--- YallaFifa/MatchRequestViewController.swift
+++ YallaFifa/MatchRequestViewController.swift
@@ -51,6 +51,7 @@ class MatchRequestViewController: UIViewController , CLLocationManagerDelegate ,
     override func viewDidLoad() {
         super.viewDidLoad()
         // Map setup
+        
         self.mapView.delegate = self
         mapView.setMinZoom(10, maxZoom: 19)
         self.mapView.isMyLocationEnabled = true
@@ -207,104 +208,7 @@ class MatchRequestViewController: UIViewController , CLLocationManagerDelegate ,
         }
     }
 
-//    // ----------------------------------------
-//    let baseURLDirections = "https://maps.googleapis.com/maps/api/directions/json?"
-//    var selectedRoute: Dictionary<NSObject, AnyObject>!
-//    var overviewPolyline: Dictionary<NSObject, AnyObject>!
-//    var originCoordinate: CLLocationCoordinate2D!
-//    var destinationCoordinate: CLLocationCoordinate2D!
-//    var originAddress: String!
-//    var destinationAddress: String!
-//    var originMarker: GMSMarker!
-//    var destinationMarker: GMSMarker!
-//    var routePolyline: GMSPolyline!
-//    
-//    
-//    
-//    func createBlueLineBetween2locations(orderedLatLong startPoint :String,endPoints:String){
-//        print("fffasdadafff")
-//        let orgin = startPoint
-//        let destination = endPoints
-//        getDirections(origin: orgin, destination: destination, waypoints: nil, travelMode: nil, completionHandler: { (status, success) -> Void in
-//            if success {
-//                self.drawRoute()
-//            }
-//            else {
-//                
-//            }
-//        })
-//        
-//    }
-//    
-//    
-//    func getDirections(origin: String!, destination: String!, waypoints: Array<String>!, travelMode: AnyObject!, completionHandler: ((_ status:   String, _ success: Bool) -> Void)) {
-//        if let originLocation = origin {
-//            if let destinationLocation = destination {
-//                var directionsURLString = baseURLDirections + "origin=" + originLocation + "&destination=" + destinationLocation
-//                
-//                directionsURLString = directionsURLString.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
-//                let directionsURL = NSURL(string: directionsURLString)
-//                
-//                DispatchQueue.main.async(execute: { () -> Void in
-//                    let directionsData = NSData(contentsOfURL: directionsURL! as URL)
-//                    
-//                    do {
-//                        if let dictionary = try JSONSerialization.JSONObjectWithData(directionsData!, options: []) as? NSDictionary {
-//                            print(dictionary)
-//                            let status = dictionary["status"] as! String
-//                            
-//                            if status == "OK" {
-//                                self.selectedRoute = (dictionary["routes"] as! Array<Dictionary<NSObject, AnyObject>>)[0]
-//                                self.overviewPolyline = self.selectedRoute["overview_polyline"] as! Dictionary<NSObject, AnyObject>
-//                                
-//                                let legs = self.selectedRoute["legs"] as! Array
-//                                    
-//                                    <Dictionary<NSObject, AnyObject>>
-//                                
-//                                let startLocationDictionary = legs[0]["start_location"] as! Dictionary<NSObject, AnyObject>
-//                                self.originCoordinate = CLLocationCoordinate2DMake(startLocationDictionary["lat"] as! Double, startLocationDictionary["lng"] as! Double)
-//                                
-//                                let endLocationDictionary = legs[legs.count - 1]["end_location"] as! Dictionary<NSObject, AnyObject>
-//                                self.destinationCoordinate = CLLocationCoordinate2DMake(endLocationDictionary["lat"] as! Double, endLocationDictionary["lng"] as! Double)
-//                                
-//                                self.originAddress = legs[0]["start_address"] as! String
-//                                self.destinationAddress = legs[legs.count - 1]["end_address"] as! String
-//                                
-//                                //self.calculateTotalDistanceAndDuration()
-//                                
-//                                completionHandler(status: status, success: true)
-//                            }
-//                            else {
-//                                completionHandler(status: status, success: false)
-//                            }
-//                            
-//                        }
-//                    } catch let error as NSError {
-//                        print(error.localizedDescription)
-//                        completionHandler("", false)
-//                    }
-//                })
-//            }
-//            else {
-//                completionHandler("Destination is nil.", false)
-//            }
-//        }
-//        else {
-//            completionHandler("Origin is nil", false)
-//        }
-//    }
-//    
-//    func drawRoute() {
-//        let route = overviewPolyline["points"] as! String
-//        let path: GMSPath = GMSPath(fromEncodedPath: route)
-//        routePolyline = GMSPolyline(path: path)
-//        routePolyline.map = self.mapView
-//    }
-//    
-//
-//
-    
-
+   
 }
 
 extension MatchRequestViewController:   UIGestureRecognizerDelegate {
